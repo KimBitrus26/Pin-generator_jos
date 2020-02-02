8f4a3a56119766d9937604953c103ec51366b8eb
KimBitrus26 <bitruschoji4real@gmail.com>
2020-02-02 08:54:22 +0100

    made changes on real.py and added a requirements.txt file

--- real.py
+++ real.py
@@ -5,7 +5,7 @@ import uuid #to generate a random pin security
 
 # init app object
 app = Flask(__name__)
-api = Api(app)
+
 
 # set up database
 app.config["SECRET_KEY"] = "123456KEY"
@@ -15,7 +15,7 @@ app.config["DATABASE_TRACK_MODIFITION"] = False
 # init db object
 db = SQLAlchemy(app)
 
-#  create user class
+# create user class
 class User(db.Model):
     __tablename__ = 'users'
     serial_number = db.Column(db.Integer, primary_key = True)
@@ -28,29 +28,26 @@ db.create_all()
 
 #route
 @app.route("/pin", methods=["POST"])
-def  create_pin():
-    data = request.get_json()
-    item = {
-            
-            "pin" : data["pin"]}
+def  create_a_pin():
     pinLength = 15
-    randompPin = uuid.uuid4().hex
-    randompPin = randompPin.upper()[0:pinLength]
-    item = randompPin
-    user = User(pin=item)
+    user = User(pin=str(uuid.uuid4().hex).upper()[0:pinLength])
     db.session.add(user)
     db.session.commit()
-    return jsonify({"msg":"pin created"})
+    return jsonify({"msg":"Pin created"})
 
 # validating a valid pin
-@app.route("/pin/<string:serial_number>", methods=["POST"])
+@app.route("/pin/<string:serial_number>", methods=["GET"])
 def  get_a_pin(serial_number):
-    pinNumber = User.query.filter_by(serial_number=serial_number).first()
-    if pinNumber:
-        return jsonify({'msg':'Valid pin number '})
-    
+    pinNum = User.query.filter_by(serial_number=serial_number).first()
+    if not pinNum:
+        return jsonify({"msg":"Invalid pin"})
+       
+    pin_data = {}
+    pin_data['serial_number'] = pinNum.serial_number
+    pin_data['pin'] = pinNum.pin
 
-    return jsonify({'msg': 'Invalid pin number'})
+    return jsonify({'Valid pin':pin_data})
+
 
 
 # run server
